935604ce9fd355a79311b21e201836a2748cf59b
Matheus Almeida <matheusjv11@hotmail.com>
2023-06-22 21:08:53 -0300

    updating components and react version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
+  // next.js config
 })
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,8 @@
+import * as S from './styles'
+
+const Home = () => (
+  <S.Wrapper>
+      <h1>Home</h1>
+  </S.Wrapper>
+)
+export default Home
