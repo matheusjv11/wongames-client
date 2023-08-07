1c131a032441bfec81e673fa9be38fea3a983303
Matheus Almeida <matheusjv11@hotmail.com>
2023-08-07 08:01:14 -0300

    Add sing-in and sign-up templates

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Auth from '.'
+
+describe('<Auth />', () => {
+  renderWithTheme(
+    <Auth title="Auth Title">
+      <input type="text" />
+    </Auth>
+  )
+
+  it('should render the heading', () => {
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument()
+
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+})
