6e3dfc7a4cf039bf73ba9699bffa84fefdb08f1a
Matheus Almeida <matheusjv11@hotmail.com>
2023-08-09 08:23:44 -0300

    Changes on Showcase component

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -1,8 +1,28 @@
+import Highlight, { HighlightProps } from 'components/Highlight'
 import * as S from './styles'
+import { GameCardProps } from 'components/GameCard'
+import Heading from 'components/Heading'
+import GameCardSlider from 'components/GameCardSlider'
 
-const Showcase = () => (
+export type ShowcaseProps = {
+  title?: string,
+  highlight?: HighlightProps,
+  games?: GameCardProps[]
+}
+
+const Showcase = ({
+  title,
+  highlight,
+  games
+}: ShowcaseProps) => (
   <S.Wrapper>
-      <h1>Showcase</h1>
-  </S.Wrapper>
+    {!!title && (
+      <Heading lineLeft lineColor="secondary">
+        { title }
+      </Heading>
+    )}
+    {!!highlight && <Highlight {...highlight} />}
+    {!!games && <GameCardSlider items={games} />}
+</S.Wrapper>
 )
 export default Showcase
--- src/components/Showcase/stories.tsx
+++ src/components/Showcase/stories.tsx
@@ -1,14 +1,46 @@
 import { Meta, StoryObj } from '@storybook/react';
 import Showcase from '.'
 
+import highlightMock from 'components/Highlight/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+
 const meta: Meta<typeof Showcase> = {
   title: 'Showcase',
   component: Showcase,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '0 auto'}}>
+        <Story />
+      </div>
+    )
+  ],
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  } 
 };
 export default meta;
 
 type Story = StoryObj<typeof Showcase>;
 
 export const Basic: Story = {
-  render: () => <Showcase />,
+  render: (args) => <Showcase {...args} />,
 };
+
+Basic.args = {
+  title: 'Most Popular',
+  highlight: highlightMock,
+  games: gamesMock
+}
+
+export const WithoutTitle: Story = {
+  render: (args) => <Showcase {...args} />,
+};
+
+WithoutTitle.args = {
+  highlight: highlightMock,
+  games: gamesMock
+}
+
